c563bbc2ae0a77f851b162cc763d151693c7a2bd
Mustafa Çoban <cobanmus15@itu.edu.tr>
2021-04-05 00:47:31 +0300

    minor change

--- wiper/sketch.js
+++ wiper/sketch.js
@@ -12,7 +12,7 @@ function draw() {
 function putCircle() {
   fill(random(256), 0, random(256), 160);
   noStroke();
-  circle(random(0, windowWidth), random(0, windowHeight), random(1, 51));
+  circle(random(windowWidth), random(windowHeight), random(1, 51));
 }
 
 function mouseDragged() {
